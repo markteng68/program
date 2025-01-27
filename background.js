65740fd9c680397e0cb975194f6ff80d44e80f16
markteng68 <mark0989085386@gmail.com>
2025-01-27 15:10:32 -0600

    $ git config --global user.name "markteng68"

---
+++ background.js
@@ -0,0 +1,8 @@
+/*chrome.runtime.onInstalled.addListener(() => {
+    console.log("Amazon Order Info Scraper installed.");
+  });
+  // 處理來自 content script 的消息
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("Message received from content script:", message);
+    sendResponse({ status: "Message processed." });
+  });*/
